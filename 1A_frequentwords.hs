3af106b40bb04c3cbacd9c3232a907300cb346cf
Michael Sloma <mmierzejewski2@gmail.com>
2014-10-03 18:05:10 -0400

    changed hashmap construction to use a fold

--- 1A_frequentwords.hs
+++ 1A_frequentwords.hs
@@ -1,4 +1,4 @@
-import qualified Data.HashMap.Lazy as HM  
+import qualified Data.HashMap.Strict as HM  
 
 --generate all sliding windows of length n in text
 windows::Int->String->[String]
@@ -8,14 +8,13 @@ windows n text | length (take n text) >= n = take n text:(windows n $ drop 1 tex
 
 --from a string and a window size, compute frequency of each unique window
 frequencies::Int->String->(HM.HashMap String Integer)
-frequencies n s = window_freq (windows n s) HM.empty
-    where window_freq [] hm = hm
-          window_freq (x:xs) hm = window_freq xs (HM.insertWith (+) x 1 hm) 
+frequencies n s = foldr step HM.empty (windows n s)
+    where step x hm = (HM.insertWith (+) x 1 hm) 
 
 --extract the most frequent strings from a map of strings
 mostFrequent::(HM.HashMap String Integer)->[String]
-mostFrequent hm = HM.keys $ HM.filter is_best hm
-              where is_best = (== maximum (HM.elems hm)) 
+mostFrequent hm = HM.keys $ best
+    where best = HM.filter (== maximum (HM.elems hm)) hm
 
 --find the most frequent kmers of length k in string text
 mostFrequentKmer::Int->String->[String]

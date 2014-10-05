cf63bc19db5191433849edba04d7424e4e96dcbc
Michael Sloma <mmierzejewski2@gmail.com>
2014-10-04 22:26:44 -0400

    find maximum G-C skew in text

---
+++ 1E_maximumskew.hs
@@ -0,0 +1,16 @@
+import qualified Data.Vector as V
+
+skew::String->V.Vector Int
+skew s = V.scanl step 0 (V.fromList s)
+	where step i c | c=='C' = i+1
+				   | c=='G' = i-1
+				   | otherwise = i
+
+maxSkew::String->V.Vector Int
+maxSkew s = V.map fst $ V.filter (\x->(snd x)==V.maximum vs) (V.indexed vs)
+	where vs = skew s
+
+main::IO()
+main = do
+	text <- getLine
+	V.mapM_ putStrLn $ V.map show $ maxSkew text

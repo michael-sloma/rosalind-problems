f7c22f5a265c86580869beb818b30f10e5398da0
Michael Sloma <mmierzejewski2@gmail.com>
2014-10-07 11:07:15 -0400

    cleanup with hlint

--- 1A_frequentwords.hs
+++ 1A_frequentwords.hs
@@ -1,19 +1,19 @@
 import qualified Data.HashMap.Strict as HM  
-
+type FreqMap = HM.HashMap String Integer
 --generate all sliding windows of length n in text
 windows::Int->String->[String]
 windows 0 _ = []
-windows n text | length (take n text) >= n = take n text:(windows n $ drop 1 text)
+windows n text | length (take n text) >= n = take n text:windows n (drop 1 text)
                | otherwise = []
 
 --from a string and a window size, compute frequency of each unique window
-frequencies::Int->String->(HM.HashMap String Integer)
+frequencies::Int->String->FreqMap
 frequencies n s = foldr step HM.empty (windows n s)
-    where step x hm = (HM.insertWith (+) x 1 hm) 
+    where step x = HM.insertWith (+) x 1
 
 --extract the most frequent strings from a map of strings
-mostFrequent::(HM.HashMap String Integer)->[String]
-mostFrequent hm = HM.keys $ best
+mostFrequent::FreqMap->[String]
+mostFrequent hm = HM.keys best
     where best = HM.filter (== maximum (HM.elems hm)) hm
 
 --find the most frequent kmers of length k in string text

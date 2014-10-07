0c06fdf1fcde3d36d8a56576bcad5296aa16f2db
Michael Sloma <mmierzejewski2@gmail.com>
2014-10-07 11:09:41 -0400

    cleanup with hlint

--- 1C_patternmatching.hs
+++ 1C_patternmatching.hs
@@ -2,12 +2,12 @@ import Data.List
 
 windows::Int->String->[String]
 windows 0 _ = []
-windows n text | length (take n text) >= n = take n text:(windows n $ drop 1 text)
+windows n text | length (take n text) >= n = take n text:windows n (drop 1 text)
                | otherwise = []
 
 patternMatching::String->String->[Int]
 patternMatching gen pat = reverse . snd $ 
-							foldl' step (0,[]) $ (windows (length pat) gen)
+							foldl' step (0,[]) $ windows (length pat) gen
 	where step (ind,acc) wndw = (ind+1,if wndw==pat then ind:acc else acc)
 
 prettify::[String]->String
